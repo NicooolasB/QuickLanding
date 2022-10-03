d69c207d410e63cf3b1301844938976ab8dcaea2
NicooolasB <nicolas.bloch1@uqac.ca>
2022-10-04 00:59:59 +0200

    addition of a first level button

--- Bloch_Nicolas_QuickLanding/Assets/Scripts/PauseMenu.cs
+++ Bloch_Nicolas_QuickLanding/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -9,15 +10,17 @@ public class PauseMenu : MonoBehaviour
     public GameObject playButton;
     public GameObject pauseButton;
     public GameObject playAgainButton;
+    public GameObject homeButton;
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPause = false;
-        playButton.SetActive(false);
         pauseButton.SetActive(true);
+        playButton.SetActive(false);
         playAgainButton.SetActive(false);
+        homeButton.SetActive(false);
 
     }
 
@@ -26,10 +29,18 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPause = true;
-        playButton.SetActive(true);
         pauseButton.SetActive(false);
+        playButton.SetActive(true);
         playAgainButton.SetActive(true);
+        homeButton.SetActive(true);
 
+    }
+
+    public void FirstLevel()
+    {
+        Time.timeScale = 1f;
+        PauseMenu.GameIsPause = false;
+        SceneManager.LoadScene(0); 
     }
     
 }
